25bdbff958c5bcd2ee6e87dafba1180ec53c62cd
ktenbrook1 <katelynntenbrook@gmail.com>
2020-11-20 15:46:23 -0600

    Made get function

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+
+const repoContext = require("./repository/repository-wrapper.js");
 const express = require('express');
 
 const app = express();
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
-   });
+});
+
+app.get("/api/products", (req, res) => {
+    let movies = repoContext.movies.findAllMovies();
+    res.send(movies);
+});
    
