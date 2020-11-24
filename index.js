83c25dec02a832eed25d2a6cf9f67d10f9452f7c
ktenbrook1 <katelynntenbrook@gmail.com>
2020-11-24 11:35:54 -0600

    Delete method complete along with additional middleware for input validation

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const repoContext = require("./repository/repository-wrapper.js");
+const validators = require("./validators/custom-validations.js");
 const express = require("express");
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => validators.body(req, res, next));
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
@@ -32,6 +32,11 @@ app.put("/api/movies", (req, res) => {
     let MovieToUpdate = req.body;
     let updateMovie = repoContext.movies.updateMovie(MovieToUpdate);
     res.send(updateMovie);
+});
+app.delete("/api/movies/:id", (req, res) => {
+    let id = req.params.id;
+    let updatedDataSet = repoContext.movies.deleteMovie(id);
+    res.send(updatedDataSet);
 });
    
    
