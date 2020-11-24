1abacdc95779b3f1882d8f9d5c174aa6834e145c
ktenbrook1 <katelynntenbrook@gmail.com>
2020-11-24 11:01:58 -0600

    Post now works

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const repoContext = require("./repository/repository-wrapper.js");
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
@@ -18,5 +22,11 @@ app.get("/api/movies/:id", (req, res) => {
     let id = req.params.id;
     let movie = repoContext.movies.findMovieById(id);
     res.send(movie);
-   });
+});
+app.post("/api/movies", (req, res) => {
+    let newMovie = req.body;
+    let addedMovie = repoContext.movies.createMovie(newMovie);
+    res.send(addedMovie);
+});
+   
    
