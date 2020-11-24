0a3a9524f23a8fc99c2e26a5f64217162e16e20e
ktenbrook1 <katelynntenbrook@gmail.com>
2020-11-24 10:43:21 -0600

    Can now get a movie by ID

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
 });
 
-app.get("/", (req, res) => {
+app.get("/api/movies", (req, res) => {
     console.log("hello");
 
     let movies = repoContext.movies.findAllMovies();
     res.send(movies);
 });
+
+app.get("/api/movies/:id", (req, res) => {
+    let id = req.params.id;
+    let movie = repoContext.movies.findMovieById(id);
+    res.send(movie);
+   });
    
